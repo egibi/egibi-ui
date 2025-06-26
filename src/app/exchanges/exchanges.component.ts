caa94f7ffc4e99dc2c94f09c26b1a639bd63a2df
Adam Hubbard <ahubbard0597@gmail.com>
2025-06-26 16:19:56 -0500

    2025_06_26_1620: Exchanges and Markets initial setup

---
+++ src/app/exchanges/exchanges.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'exchanges',
+  imports: [],
+  templateUrl: './exchanges.component.html',
+  styleUrl: './exchanges.component.scss'
+})
+export class ExchangesComponent {
+
+}
